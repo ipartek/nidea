7eeb05d80aea1f85de84f49401013fd9a9710e33
Curso <Curso@Formacion-TOSH7.ipartekaula.com>
2018-04-24 11:19:44 +0200

    Añadir usuarios desde fichero

--- src/main/java/com/ipartek/formacion/nidea/migracion/CrearUsuariosFromFileText.java
+++ src/main/java/com/ipartek/formacion/nidea/migracion/CrearUsuariosFromFileText.java
@@ -9,15 +9,27 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.ipartek.formacion.nidea.pojo.Usuario;
+import com.mysql.jdbc.MysqlDataTruncation;
 import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+import com.mysql.jdbc.log.Log;
 
 public class CrearUsuariosFromFileText {
 
-	private static final String FILENAME = "C:\\Desarrollo\\jee-oxygen\\workspace2\\nidea\\doc\\personas_little.txt";
+	private static final String FILENAME = "C:\\Desarrollo\\jee-oxygen\\workspace2\\nidea\\doc\\personas.txt";
+
+	// Base de datos
+	private static final String URL = "jdbc:mysql://localhost/nidea?user=root&password=root";
+	private static final String SQL = "INSERT INTO `usuario` (`nombre`, `password`, `id_rol`, email) VALUES ( ?, '123456', '2', ?);";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+	private static final int NUMERO_CAMPOS = 7;
+	private static final int EDAD_MINIMA = 18;
 
 	public static void main(String[] args) throws SQLException {
 
-		int error_duplicados = 0;
+		int error_email = 0;
+		int error_nombre = 0;
+		int error_nombre_largo = 0;
 		int linea = 0;
 		int insertados = 0;
 		int error_campos = 0;
@@ -25,37 +37,42 @@ public class CrearUsuariosFromFileText {
 
 		String sCurrentLine;
 		Usuario usuario = null;
-		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 		String campos[];
+		String exc[];
 
 		System.out.println("Crear usuarios desde un fichero de texto");
-		final String URL = "jdbc:mysql://localhost/nidea?user=root&password=root";
-		final String sql = "INSERT INTO `usuario` (`nombre`, `password`, `id_rol`, email) VALUES ( ?, '123456', '2', ?);";
 
 		Connection con = null;
 		PreparedStatement pst = null;
+
 		try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
 
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(DRIVER);
 			con = DriverManager.getConnection(URL);
 
 			// es importante esto para que no haga autocommit y poder hacer el rollback
 			con.setAutoCommit(false);
 
-			pst = con.prepareStatement(sql);
+			pst = con.prepareStatement(SQL);
 
 			while ((sCurrentLine = br.readLine()) != null) {
 
-				usuario = new Usuario();
 				campos = sCurrentLine.split(",");
 
 				try {
-					if (campos.length == 7) {
-						usuario.setNombre(campos[0] + " " + campos[1] + " " + campos[2]);
-						usuario.setEmail(campos[4]);
-						if (Integer.parseInt(campos[3]) >= 18) {
-							usuarios.add(usuario);
+					if (campos.length != NUMERO_CAMPOS) {
+						error_campos++;
+						
+					} else {
+						// se mapea el nombre con los tres primeros campos que son nombres y apellidos
+						usuario = mapear(campos);
 
+						// Comprueba si los usuarios tienen la edad minima para entrar en la bbdd
+						if (!(Integer.parseInt(campos[3]) >= EDAD_MINIMA)) {
+							error_edad++;
+							
+						} else {							
+							insertados++;
 							pst.setString(1, usuario.getNombre());
 							pst.setString(2, usuario.getEmail());
 
@@ -63,34 +80,48 @@ public class CrearUsuariosFromFileText {
 								System.out.println("Usuario" + usuario.getNombre() + " insertado");
 							} else {
 								System.out.println("******Error al insertar");
-							}
-
-							System.out.println("linea " + linea + "=> " + usuario);
-							insertados++;
-						} else {
-							error_edad++;
-						}
-					} else {
-						error_campos++;
+							}							
+						} 
 					}
-				} catch (MySQLIntegrityConstraintViolationException e) {
-					error_duplicados++;
-					e.printStackTrace();
-					System.out.println(e.getLocalizedMessage());
 				}
 
+				// Comprueba si hay nombres o email duplicados
+				catch (MySQLIntegrityConstraintViolationException e) {
+
+					exc = e.getLocalizedMessage().split("'");
+					// System.out.println(e.getLocalizedMessage());
+					if ("email".equals(exc[exc.length - 1])) {
+						error_email++;
+					} else if ("nombre_UNIQUE".equals(exc[exc.length - 1])) {
+						error_nombre++;
+					}
+				}
+				
+				// Comprueba si los campos son demasiado largos
+				catch (MysqlDataTruncation e) {
+					error_nombre_largo++;
+					// System.out.println(e.getLocalizedMessage());
+				}
 				linea++;
-			}
+				
+			}//while
 
 			// comitar cambios al terminar el proceso
 			con.commit();
-			
+
 			System.out.println("Lineas leidas " + linea);
 			System.out.println("Lineas correctas " + insertados);
-			System.out.println(usuarios);
+			// System.out.println(usuarios);
 			System.out.println("Errores de campos " + error_campos);
 			System.out.println("Errores de edad " + error_edad);
-			System.out.println("Errores de duplicidad " + error_duplicados);
+			System.out.println("Errores de duplicidad de nombre " + error_nombre);
+			System.out.println("Errores de nombre demasiado largo " + error_nombre_largo);
+			System.out.println("Errores de duplicidad de email " + error_email);
+
+			System.out.println(
+					"Lineas erroneas " + (error_campos + error_edad + error_nombre + error_nombre_largo + error_email));
+			System.out.println("Lineas Totales "
+					+ (insertados + error_campos + error_edad + error_nombre + error_nombre_largo + error_email));
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -105,6 +136,18 @@ public class CrearUsuariosFromFileText {
 				con.close();
 			}
 		}
+	}
+
+	/**
+	 * Devuelve un usuario con los 3 primeros campos mapeados para el nombre y el email con el 5
+	 * @param campos
+	 * @return
+	 */
+	private static Usuario mapear(String[] campos) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre(campos[0] + " " + campos[1] + " " + campos[2]);
+		usuario.setEmail(campos[4]);
+		return usuario;
 	}
 
 }
